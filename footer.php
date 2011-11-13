1fc2f9a8796313c64d18d9b8b24b5b12fa720d0c
Robert Reeve <deniaz@cheekyowl.com>
2011-11-13 16:07:58 +0100

    Added topic/tag navigator

--- footer.php
+++ footer.php
@@ -23,20 +23,52 @@ $options = get_option( 'geekhole_theme_options' );
 	<div class="span-24 separator"></div>
 		
 	<div id="topic-navigator" class="span-24">
-		<ul>
+		<div id="topic-list" class="span-12">
 			<?php
+				$counter = 1;
 				foreach ($mainCategories as $main)
 				{
 					$args['parent'] = $main->term_id;
 					$subCategories = get_categories($args);
-					echo "<li><strong>{$main->name}</strong></li>";
+					
+					$class = 'span-6';
+					$link = get_category_link($main->term_id);
+					if ($counter % 2 == 0)
+					{
+						$class = $class . ' last';
+					}
+					echo "<div class=\"{$class}\"><ul>";
+					echo "<li class=\"main-category\"><a href=\"{$link}\">{$main->name}</a></li>";
 					foreach ($subCategories as $sub)
 					{
-						echo "<li><em>{$sub->name}</em></li>";
+						$link = get_category_link($sub->term_id);
+						echo "<li class=\"sub-category\"><a href=\"{$link}\">{$sub->name}</a></li>";
 					}
+					echo "</ul></div>";
+					$counter++;
 				} 
 			?>
-		</ul>
+		</div>
+		<div id="tag-cloud" class="span-12 last">
+			<?php
+				$tags = get_tags();
+				
+				$max;
+				
+				foreach ($tags as $tag)
+				{
+					$max = $max + $tag->count;
+				}
+				
+				foreach ($tags as $tag)
+				{
+					$link = get_tag_link($tag->term_id);
+					$rank = round(($tag->count / $max * 100000) / 10);
+					
+					echo "<a href=\"{$link}\" title=\"{$tag->count} Tags\" style=\"font-size: {$rank}%\";> {$tag->name} </a>";
+				}
+			?>
+		</div>
 	</div>
 	
 	<div class="span-24 separator-label"><span>Impressum</span></div>
