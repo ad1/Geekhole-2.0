2bbd54b864fc5e8d1a3a9bc8c36f0a4187ae966d
Robert Reeve <deniaz@cheekyowl.com>
2011-11-13 15:33:37 +0100

    Styled comments

--- comments.php
+++ comments.php
@@ -1,2 +1,103 @@
 <div class="span-24 separator-label"><span>Kommentare</span></div>
 <div class="span-24 separator"></div>
+
+
+<?php
+
+	if (!empty($_SERVER['SCRIPT_FILENAME']) && 'comments.php' == basename($_SERVER['SCRIPT_FILENAME']))
+		die ('Request killed by geekhole <3');
+
+	if ( post_password_required() ) { ?>
+		Post requires password.
+	<?php
+		return;
+	}
+?>
+
+<div class="span-24" id="comment-wrapper">
+<?php if ( have_comments() ) : ?>
+
+	<div class="navigation">
+		<div class="next-posts"><?php previous_comments_link() ?></div>
+		<div class="prev-posts"><?php next_comments_link() ?></div>
+	</div>
+
+	<ol class="commentlist">
+		<?php wp_list_comments(); ?>
+	</ol>
+
+	<div class="navigation">
+		<div class="next-posts"><?php previous_comments_link() ?></div>
+		<div class="prev-posts"><?php next_comments_link() ?></div>
+	</div>
+	<hr />
+	
+ <?php else : // this is displayed if there are no comments so far ?>
+
+	<?php if ( comments_open() ) : ?>
+
+	 <?php else : // comments are closed ?>
+		<p>Für diesen Artikel werden keine Kommentare entgegen genommen.</p>
+
+	<?php endif; ?>
+	
+<?php endif; ?>
+
+<?php if ( comments_open() ) : ?>
+
+<div id="respond">
+
+	<div class="cancel-comment-reply">
+		<?php cancel_comment_reply_link(); ?>
+	</div>
+
+	<?php if ( get_option('comment_registration') && !is_user_logged_in() ) : ?>
+		<p>You must be <a href="<?php echo wp_login_url( get_permalink() ); ?>">logged in</a> to post a comment.</p>
+	<?php else : ?>
+
+	<form action="<?php echo get_option('siteurl'); ?>/wp-comments-post.php" method="post" id="commentform">
+
+		<?php if ( is_user_logged_in() ) : ?>
+
+			<p>Eingeloggt als <a href="<?php echo get_option('siteurl'); ?>/wp-admin/profile.php"><?php echo $user_identity; ?></a>. <a href="<?php echo wp_logout_url(get_permalink()); ?>" title="Log out of this account">Log out &raquo;</a></p>
+
+		<?php else : ?>
+			<div class="span-10">
+				<div>
+					<input type="text" name="author" id="author" value="<?php echo esc_attr($comment_author); ?>" size="22" tabindex="1" <?php if ($req) echo "aria-required='true'"; ?> />
+					<label for="author">Name <?php if ($req) echo "*"; ?></label>
+				</div>
+	
+				<div>
+					<input type="text" name="email" id="email" value="<?php echo esc_attr($comment_author_email); ?>" size="22" tabindex="2" <?php if ($req) echo "aria-required='true'"; ?> />
+					<label for="email">Mail (nicht &ouml;ffentlich sichtbar) <?php if ($req) echo "*"; ?></label>
+				</div>
+	
+				<div>
+					<input type="text" name="url" id="url" value="<?php echo esc_attr($comment_author_url); ?>" size="22" tabindex="3" />
+					<label for="url">Website</label>
+				</div>
+				
+				<input name="submit" type="submit" id="submit" tabindex="5" value="Kommentieren" />
+			</div>
+
+		<?php endif; ?>
+
+		<div class="span-12 last">
+			<textarea name="comment" id="comment" cols="58" rows="10" tabindex="4"></textarea>
+		</div>
+
+		<div>
+			<?php comment_id_fields(); ?>
+		</div>
+		
+		<?php do_action('comment_form', $post->ID); ?>
+
+	</form>
+
+	<?php endif; // If registration required and not logged in ?>
+	
+</div>
+
+<?php endif; ?>
+</div>
