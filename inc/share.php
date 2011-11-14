5ae80d6f27f321a4ba0301f5eec9e5d252330f21
Robert Reeve <deniaz@cheekyowl.com>
2011-11-14 14:25:41 +0100

    whoopsie, forgot the include file

---
+++ inc/share.php
@@ -0,0 +1,48 @@
+<?php
+$output = array();
+if ($isFrontpage === true)
+{
+	/**
+	 * Because this template get's included, $isFrontpage will be either TRUE or FALSE. 
+	 * If $isFrontpage is TRUE, we'll add URL which should be liked and is defined by $perma
+	 */
+	
+	$output['facebook'] = "<div class=\"social-item facebook\"><div class=\"fb-like\" data-send=\"false\"
+										data-layout=\"button_count\" data-width=\"450\" 
+										data-show-faces=\"false\" data-href=\"{$perma}\"></div></div>";
+	
+	$output['twitter'] = "<div class=\"social-item twitter\"><a href=\"https://twitter.com/share\" 
+							 class=\"twitter-share-button\" 
+							 data-count=\"horizontal\" 
+							 data-via=\"geekhole_ch\"
+							 data-url=\"{$perma}\" 
+							 data-lang=\"de\">
+							Tweet
+						  </a></div>";
+	
+	$output['google'] = "<div class=\"social-item google\"><g:plusone size=\"medium\" href=\"{$perma}\"></g:plusone></div>";
+}
+else {
+	$output['facebook'] = "<div class=\"social-item facebook\"><div class=\"fb-like\" data-send=\"false\"
+										data-layout=\"button_count\" data-width=\"450\" 
+										data-show-faces=\"false\"></div></div>";
+	
+	$output['twitter'] = "<div class=\"social-item twitter\"><a href=\"https://twitter.com/share\"
+								 class=\"twitter-share-button\" 
+								 data-count=\"horizontal\" 
+								 data-via=\"geekhole_ch\"
+								 data-lang=\"de\">
+								Tweet
+							  </a></div>";
+	
+	$output['google'] = "<div class=\"social-item google\"><g:plusone size=\"medium\"></g:plusone></div>";
+	
+}
+
+// Now looping through social media stuff
+
+	foreach($output as $item)
+	{
+		echo $item;
+	}
+?>
