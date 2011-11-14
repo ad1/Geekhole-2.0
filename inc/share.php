20e4ce80667e04d5d0657b35d1d911f231ff2564
Robert Reeve <deniaz@cheekyowl.com>
2011-11-14 14:27:43 +0100

    Changed icon-order - thx facebook

--- inc/share.php
+++ inc/share.php
@@ -6,11 +6,6 @@ if ($isFrontpage === true)
 	 * Because this template get's included, $isFrontpage will be either TRUE or FALSE. 
 	 * If $isFrontpage is TRUE, we'll add URL which should be liked and is defined by $perma
 	 */
-	
-	$output['facebook'] = "<div class=\"social-item facebook\"><div class=\"fb-like\" data-send=\"false\"
-										data-layout=\"button_count\" data-width=\"450\" 
-										data-show-faces=\"false\" data-href=\"{$perma}\"></div></div>";
-	
 	$output['twitter'] = "<div class=\"social-item twitter\"><a href=\"https://twitter.com/share\" 
 							 class=\"twitter-share-button\" 
 							 data-count=\"horizontal\" 
@@ -21,12 +16,13 @@ if ($isFrontpage === true)
 						  </a></div>";
 	
 	$output['google'] = "<div class=\"social-item google\"><g:plusone size=\"medium\" href=\"{$perma}\"></g:plusone></div>";
-}
-else {
-	$output['facebook'] = "<div class=\"social-item facebook\"><div class=\"fb-like\" data-send=\"false\"
-										data-layout=\"button_count\" data-width=\"450\" 
-										data-show-faces=\"false\"></div></div>";
 	
+	$output['facebook'] = "<div class=\"social-item facebook\"><div class=\"fb-like\" data-send=\"false\"
+											data-layout=\"button_count\" data-width=\"450\" 
+											data-show-faces=\"false\" data-href=\"{$perma}\"></div></div>";
+}
+else
+{	
 	$output['twitter'] = "<div class=\"social-item twitter\"><a href=\"https://twitter.com/share\"
 								 class=\"twitter-share-button\" 
 								 data-count=\"horizontal\" 
@@ -36,6 +32,10 @@ else {
 							  </a></div>";
 	
 	$output['google'] = "<div class=\"social-item google\"><g:plusone size=\"medium\"></g:plusone></div>";
+	
+	$output['facebook'] = "<div class=\"social-item facebook\"><div class=\"fb-like\" data-send=\"false\"
+											data-layout=\"button_count\" data-width=\"450\" 
+											data-show-faces=\"false\"></div></div>";
 	
 }
 
