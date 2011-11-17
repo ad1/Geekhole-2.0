306dde243bbf08add4c240c507df13976f3b2cf6
Robert Reeve <deniaz@cheekyowl.com>
2011-11-17 10:40:22 +0100

    fixed merge issue

--- js/geekhole.js
+++ js/geekhole.js
@@ -2,16 +2,6 @@
 	
 	var GH = GH || {};
 	
-	GH.backLinker = {
-		init: function() {
-			if ($('#back-to-previous').length > 0) {
-				$('#back-to-previous').click(function() {
-					console.log(history);
-				});
-			}
-		},
-	};
-	
 	GH.imageZoom = {
 			init: function() {
 				$('.image-zoom, .wp-caption a').fancybox();
@@ -20,11 +10,7 @@
 	
 	$(document).ready(function() {
 		$('#social-bubble').widgetize();
-<<<<<<< HEAD
-		GH.backLinker.init();
-=======
 		$('input#s').searchify();
->>>>>>> 0f12185... Added JS to handle search-input (empty, default text, ...)
 		GH.imageZoom.init();
 	});
 })(jQuery);
